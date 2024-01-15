8c96a463d07f87fb0e67ad6ec092314f25554201
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-01-15 16:12:42 +0530

    Nav bar creating in progress

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,6 +22,16 @@ const Navbar = () => (
      <div />
      <a href="/" className='p__opensans'>Book Now</a>
     </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => {}}/>
+      <ul className='app__navbar-smallscreen-links'>
+      <li className='p__opensans'><a href="#home">Home</a></li>
+      <li className='p__opensans'><a href="#about">About</a></li>
+      <li className='p__opensans'><a href="#menu">Menu</a></li>
+      <li className='p__opensans'><a href="#awards">Awards</a></li>
+      <li className='p__opensans'><a href="#contact ">Contact</a></li>
+    </ul>
+    </div>
   </nav>
 );
 

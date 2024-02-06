e64abc3126060c70d93279190a9d3e42cb665206
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-02-07 00:01:24 +0530

    Gallery created

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import {BsInstagram,BsArrowLeftShort,BsArrowRightShort} from "react-icons/bs";
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
 import { SubHeading } from "../../components";
-import { images} from "../../constants";
+import { images } from "../../constants";
 import "./Gallery.css";
 
-const galleryImages = [images.gallery01, images.gallery02, images.gallery03,images.gallery04];
+const galleryImages = [
+  images.gallery01,
+  images.gallery02,
+  images.gallery03,
+  images.gallery04,
+];
 
 const Gallery = () => {
   const scrollRef = React.useRef(null);
@@ -33,10 +42,13 @@ const Gallery = () => {
       </div>
       <div className="app__gallery-images">
         <div className="app__gallery-images_container" ref={scrollRef}>
-          {galleryImages.map((image,index) => (
-            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+          {galleryImages.map((image, index) => (
+            <div
+              className="app__gallery-images_card flex__center"
+              key={`gallery_image-${index + 1}`}
+            >
               <img src={image} alt="gallery" />
-              <BsInstagram className='gallery__image-icon' />
+              <BsInstagram className="gallery__image-icon" />
             </div>
           ))}
         </div>
